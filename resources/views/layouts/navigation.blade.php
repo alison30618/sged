b47408824ca329d8f58a6a410700bd39156c7bf0
garciavidalf9@gmail.com <garciavidalf9@gmail.com>
2024-04-23 22:13:47 -0500

    Configuracion Navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,6 +16,21 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('equipos.index')" :active="request()->routeIs('dashboard')">
+                        {{ __('Equipos') }}
+                    </x-nav-link>
+                </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('eventos.index')" :active="request()->routeIs('dashboard')">
+                        {{ __('Eventos') }}
+                    </x-nav-link>
+                </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('participaciones.index')" :active="request()->routeIs('dashboard')">
+                        {{ __('Participaciones') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
